bb2b15f398459a726730b7288b689c586e62314e
arilizirov <109600281+arilizirov@users.noreply.github.com>
2022-09-03 21:19:02 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import Pages from './pages/Pages';
+import Category from "./components/Category";
+import {BrowserRouter} from 'react-router-dom';
+import Search from "./components/Search";
+import styled from "styled-components";
+import {Link} from "react-router-dom";
+import { GiKnifeFork } from "react-icons/gi";
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Nav>
+        <GiKnifeFork />
+        <Logo to={"/"}>delicioso</Logo>
+      </Nav>
+      <Search />
+      <Category />
+      <Pages />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+const Logo = styled(Link)`
+text-decoration: none;
+font-size: 1.5rem;
+font-weight: 400;
+font-family: 'Lobster', cursive;
+`;
+
+const Nav = styled.div`
+padding: 4rem 0rem;
+display: flex;
+justify-content: flex-start;
+align-items: center;
+svg{
+  font-size: 2rem;
+}
+`;
+
+export default App;
+
+//to fix: Japanese page not working, search bar not working, text in search bar, sliders 1:44
